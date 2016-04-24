1ecbe204998ae0b22f6e7a4163d1caba8eb029a9
MeePwn <maybewaityou@gmail.com>
2016-04-24 10:12:33 +0800

    fix issue

--- app/src/main/java/com/llbt/meepwn/lincoinblock/framework/base/BaseActivity.java
+++ app/src/main/java/com/llbt/meepwn/lincoinblock/framework/base/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.llbt.meepwn.lincoinblock.framework.base;
 
+import android.annotation.SuppressLint;
 import android.databinding.ObservableField;
 import android.support.v4.app.FragmentActivity;
 
@@ -15,6 +16,7 @@ import com.llbt.meepwn.lincoinblock.framework.ViewModel;
  * date: 16/4/12 上午10:50
  * desc:
  */
+@SuppressLint("Registered")
 public class BaseActivity<M extends Model, VM extends ViewModel, S extends Service> extends FragmentActivity {
 
     protected ObservableField<M> model;
