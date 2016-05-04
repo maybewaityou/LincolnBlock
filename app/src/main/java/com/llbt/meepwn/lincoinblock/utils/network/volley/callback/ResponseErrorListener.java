f962d10a276dd8a26758d7b088f81805c459a8c8
MeePwn <maybewaityou@gmail.com>
2016-05-04 09:57:50 +0800

    add desc

--- app/src/main/java/com/llbt/meepwn/lincoinblock/utils/network/volley/callback/ResponseErrorListener.java
+++ app/src/main/java/com/llbt/meepwn/lincoinblock/utils/network/volley/callback/ResponseErrorListener.java
@@ -4,8 +4,12 @@ package com.llbt.meepwn.lincoinblock.utils.network.volley.callback;
 import com.llbt.meepwn.lincoinblock.library.volley.VolleyError;
 
 /**
- * Created by MeePwn on 15/8/5.
- * * 返回失败回调（可以自定义返回结果）
+ * package: com.llbt.meepwn.lincoinblock.utils.network.volley
+ * author: MeePwn
+ * email: maybewaityou@gmail.com
+ * github: https://github.com/maybewaityou
+ * date: 16/4/25 上午6:11
+ * desc: 返回失败回调(可以自定义返回结果)
  */
 public interface ResponseErrorListener {
 
