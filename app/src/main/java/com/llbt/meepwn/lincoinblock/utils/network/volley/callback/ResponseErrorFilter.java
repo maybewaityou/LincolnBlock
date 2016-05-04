82fe1a953b7fad782c56b74a8927eafeec10e734
MeePwn <maybewaityou@gmail.com>
2016-05-04 09:59:22 +0800

    add desc

--- app/src/main/java/com/llbt/meepwn/lincoinblock/utils/network/volley/callback/ResponseErrorFilter.java
+++ app/src/main/java/com/llbt/meepwn/lincoinblock/utils/network/volley/callback/ResponseErrorFilter.java
@@ -3,9 +3,15 @@ package com.llbt.meepwn.lincoinblock.utils.network.volley.callback;
 import org.json.JSONObject;
 
 /**
- * Created by MeePwn on 15/9/2.
- *
+ * package: com.llbt.meepwn.lincoinblock.utils.network.volley
+ * author: MeePwn
+ * email: maybewaityou@gmail.com
+ * github: https://github.com/maybewaityou
+ * date: 16/4/25 上午6:11
+ * desc: 返回成功回调(可以过滤返回结果)
  */
 public interface ResponseErrorFilter {
+
     void onErrorFilter(JSONObject errorResponse);
+    
 }
