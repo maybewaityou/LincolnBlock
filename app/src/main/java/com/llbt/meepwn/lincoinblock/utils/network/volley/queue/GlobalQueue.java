5a03a732e1742dca45c1cb6d9eef1cb4130720ec
MeePwn <maybewaityou@gmail.com>
2016-05-04 09:55:46 +0800

    add desc

--- app/src/main/java/com/llbt/meepwn/lincoinblock/utils/network/volley/queue/GlobalQueue.java
+++ app/src/main/java/com/llbt/meepwn/lincoinblock/utils/network/volley/queue/GlobalQueue.java
@@ -11,7 +11,7 @@ import com.llbt.meepwn.lincoinblock.library.volley.toolbox.Volley;
  * email: maybewaityou@gmail.com
  * github: https://github.com/maybewaityou
  * date: 16/5/3 上午9:20
- * desc: Volley全局队列
+ * desc: Volley全局队列(单例)
  */
 public class GlobalQueue {
 
