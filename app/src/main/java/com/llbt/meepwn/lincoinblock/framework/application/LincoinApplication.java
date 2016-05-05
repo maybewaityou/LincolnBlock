44e8b6ec54da4bcfec580c01f01f831d3da6c12a
MeePwn <maybewaityou@gmail.com>
2016-05-05 17:13:41 +0800

    add LincoinApplication

---
+++ app/src/main/java/com/llbt/meepwn/lincoinblock/framework/application/LincoinApplication.java
@@ -0,0 +1,20 @@
+package com.llbt.meepwn.lincoinblock.framework.application;
+
+import android.app.Application;
+
+/**
+ * package: com.llbt.meepwn.lincoinblock.framework.application
+ * author: MeePwn
+ * email: maybewaityou@gmail.com
+ * github: https://github.com/maybewaityou
+ * date: 16/5/5 下午5:08
+ * desc: LincoinApplication
+ */
+public class LincoinApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
